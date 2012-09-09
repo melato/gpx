32084cfec0e64583e753bfe58c5f681b4c16abf0
Alex Athanasopoulos <alex@melato.org>
2012-09-09 12:32:04 +0300

    added get/set methods

--- src/main/java/org/melato/gpx/Route.java
+++ src/main/java/org/melato/gpx/Route.java
@@ -1,12 +1,50 @@
 package org.melato.gpx;
 
+import java.util.List;
 
 
 public class Route implements Cloneable {
 	public Sequence path;
 	public String name;
   
-	@Override
+	
+  public Route() {
+    super();
+  }
+
+	public Route(Sequence path, String name) {
+    super();
+    this.path = path;
+    this.name = name;
+  }
+	
+  public Route(List<Waypoint> waypoints) {
+    super();
+    this.path = new Sequence(waypoints);
+  }
+  
+  public String getName() {
+    return name;
+  }
+	
+	public List<Waypoint> getWaypoints() {
+	  return path.getWaypoints();
+	}
+
+	
+  public Sequence getPath() {
+    return path;
+  }
+
+  public void setPath(Sequence path) {
+    this.path = path;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
   public Route clone() {
     Route copy;
     try {
