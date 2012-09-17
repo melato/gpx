127702f62932b0b2b13c4d427c1dbcbcade7f4a9
Alex Athanasopoulos <alex@melato.org>
2012-09-17 12:20:22 +0300

    moved the actual distance computation back to earth.

--- src/main/java/org/melato/gpx/Earth.java
+++ src/main/java/org/melato/gpx/Earth.java
@@ -3,15 +3,29 @@ package org.melato.gpx;
 public class Earth {
   /** Earth's circumference in meters */
 	public static final float CIRCUMFERENCE = 40044000;
-	public static final Metric DISTANCE = new GlobalDistance();
 
-	/**
-	 * Compute the distance between two points in meters.  Does not use elevation.
-	 * @return
-	 */
-	public static float distance( Point p1, Point p2 ) {
-	  return DISTANCE.distance(p1, p2);
-	}
+  private static double square(double x) {
+    return x * x;
+  }
+
+  /**
+   * Haversine formula
+   * http://en.wikipedia.org/wiki/Great-circle_distance
+   * */
+  public static float distance(Point p1, Point p2) {
+    double lat1 = Math.toRadians(p1.lat);
+    double lat2 = Math.toRadians(p2.lat);
+    double lon1 = Math.toRadians(p1.lon);
+    double lon2 = Math.toRadians(p2.lon);
+    double d = square(Math.sin((lat1-lat2)/2)) + Math.cos(lat1)*Math.cos(lat2)*square(Math.sin((lon1-lon2)/2));
+    d = 2 * Math.asin(Math.sqrt(d));
+    /*
+    double d = Math.sin(lat1)*Math.sin(lat2) +
+        Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon1-lon2);
+    double d = Math.acos(d);
+     */
+    return (float) (d * CIRCUMFERENCE / (2 * Math.PI));
+  }
 
 	/** Compute the latitude difference in degrees for a given distance on the same meridian.
 	 * @param distance in meters
--- src/main/java/org/melato/gpx/GlobalDistance.java
+++ src/main/java/org/melato/gpx/GlobalDistance.java
@@ -3,28 +3,9 @@ package org.melato.gpx;
 
 public class GlobalDistance implements Metric {
 
-  private static double square(double x) {
-    return x * x;
-  }
-
-  /**
-   * Haversine formula
-   * http://en.wikipedia.org/wiki/Great-circle_distance
-   * */
   @Override
   public float distance(Point p1, Point p2) {
-    double lat1 = Math.toRadians(p1.lat);
-    double lat2 = Math.toRadians(p2.lat);
-    double lon1 = Math.toRadians(p1.lon);
-    double lon2 = Math.toRadians(p2.lon);
-    double d = square(Math.sin((lat1-lat2)/2)) + Math.cos(lat1)*Math.cos(lat2)*square(Math.sin((lon1-lon2)/2));
-    d = 2 * Math.asin(Math.sqrt(d));
-    /*
-    double d = Math.sin(lat1)*Math.sin(lat2) +
-        Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon1-lon2);
-    double d = Math.acos(d);
-     */
-    return (float) (d * Earth.CIRCUMFERENCE / (2 * Math.PI));
+    return Earth.distance(p1, p2);
   }
 
 }
