dc866830ecb95c05d1062985d5f4f0161dadf975
Alex Athanasopoulos <alex@melato.org>
2014-09-16 18:18:09 +0300

    cleaned imports

--- src/main/java/org/melato/gpx/Waypoint.java
+++ src/main/java/org/melato/gpx/Waypoint.java
@@ -20,8 +20,6 @@ package org.melato.gpx;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.melato.gps.GpsPoint;
 import org.melato.gps.Point2D;
